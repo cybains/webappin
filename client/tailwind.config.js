c0855dff6130dc701ebfaad922347862d548c587
cybains <96011867+cybains@users.noreply.github.com>
2025-04-29 23:12:27 +0200

    29/4/11/12
    last push of the night

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,20 @@
 // tailwind.config.js
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  darkMode: 'media',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)',
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
